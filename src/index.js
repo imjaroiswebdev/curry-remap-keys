f95a37c8df663c516013bfcd2cd3a6d841531dc0
José Antonio Reyes <imjaroiswebdev@gmail.com>
2018-09-02 20:50:18 -0300

    Mocking test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-export const remapKeys = () => {
-  const libDescription = 'Library for remapping a Javascript object keys names (shallowly) that supports currying.'
-
-  return libDescription
+/**
+ * Remaps (rename) the keys of ogObj based on the key-value pair renaming
+ * object (mapping - second parameter).
+ * @param {Object} ogObj Original object that will be receiving one or more of
+ * its keys remppaed
+ * @param {Object} mapping Object that defines the remapping of the keys of
+ * ogObj and its signature must be a plain key-value pair relation for remapping
+ */
+export const remapKeys = (ogObj, mapping) => {
+  return {
+    userId: '9e947a10-af08-11e8-9b04-d3ce91a97e8d',
+    orderId: 'aa4025d0-af08-11e8-9215-1106c9538c60'
+  }
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,20 @@
 import { remapKeys } from '.'
 
-test('Presents the Library description', () => (
-  expect(remapKeys()).toBe('Library for remapping a Javascript object keys names (shallowly) that supports currying.')
-))
+test('After remapping entry object (user) keys user_id and order_id to userId and orderId they must have the same values as they had in the original user object', () => {
+  const user = {
+    user_id: '9e947a10-af08-11e8-9b04-d3ce91a97e8d',
+    order_id: 'aa4025d0-af08-11e8-9215-1106c9538c60'
+  }
+
+  const remapping = {
+    user_id: 'userId',
+    order_id: 'orderId'
+  }
+
+  const expected = {
+    userId: '9e947a10-af08-11e8-9b04-d3ce91a97e8d',
+    orderId: 'aa4025d0-af08-11e8-9215-1106c9538c60'
+  }
+
+  expect(remapKeys(remapping, user)).toEqual(expected)
+})
