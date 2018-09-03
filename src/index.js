f8804974f8d0ace7a537c59c66796bce8038b755
José Antonio Reyes <imjaroiswebdev@gmail.com>
2018-09-02 22:25:08 -0300

    Remaps an object keys based on a mapping hash
    The mapping hash must be a key-value pair object

--- src/index.js
+++ src/index.js
@@ -6,9 +6,30 @@
  * @param {Object} mapping Object that defines the remapping of the keys of
  * ogObj and its signature must be a plain key-value pair relation for remapping
  */
-export const remapKeys = (ogObj, mapping) => {
-  return {
-    userId: '9e947a10-af08-11e8-9b04-d3ce91a97e8d',
-    orderId: 'aa4025d0-af08-11e8-9215-1106c9538c60'
+export const remapKeys = (mapping, ogObj) => {
+  // TODO: Add parameters validation
+  // TODO: Add currying support
+  let remappedObj = {}
+  let cleanObj = {}
+  const blacklist = Object.keys(mapping)
+  const ogKeys = Object.keys(ogObj)
+
+  Object.keys(mapping).forEach(remapper) // Will apply the mapping
+  ogKeys.forEach(cleaner) // Will clean the result of the original keys
+
+  function remapper (key) {
+    remappedObj[mapping[key]] = ogObj[key]
   }
+
+  function cleaner (key) {
+    if (blacklist.includes(key)) {
+      return
+    }
+
+    cleanObj[key] = ogObj[key]
+  }
+
+  const updatedObj = Object.assign({}, cleanObj, remappedObj)
+
+  return updatedObj
 }
--- src/index.test.js
+++ src/index.test.js
@@ -18,3 +18,26 @@ test('After remapping entry object (user) keys user_id and order_id to userId an
 
   expect(remapKeys(remapping, user)).toEqual(expected)
 })
+
+test('After remapping the resulting object must maintain its keys not being remapped ontouched', () => {
+  const user = {
+    user_id: '9e947a10-af08-11e8-9b04-d3ce91a97e8d',
+    order_id: 'aa4025d0-af08-11e8-9215-1106c9538c60',
+    productName: 'The best notebook of the whole world',
+    qty: '1'
+  }
+
+  const remapping = {
+    user_id: 'userId',
+    order_id: 'orderId'
+  }
+
+  const expected = {
+    userId: '9e947a10-af08-11e8-9b04-d3ce91a97e8d',
+    orderId: 'aa4025d0-af08-11e8-9215-1106c9538c60',
+    productName: 'The best notebook of the whole world',
+    qty: '1'
+  }
+
+  expect(remapKeys(remapping, user)).toEqual(expected)
+})
